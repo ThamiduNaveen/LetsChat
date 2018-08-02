73c423c44c6f1a9ab8e85618dfc9cb185a8d3f9b
thamiduNaveen <thamidunaveen@gmail.com>
2018-08-02 13:29:05 +0530

    contain login register pages

--- app/src/main/java/com/example/asus_pc/letschat/StartPageActivity.java
+++ app/src/main/java/com/example/asus_pc/letschat/StartPageActivity.java
@@ -1,13 +1,40 @@
 package com.example.asus_pc.letschat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPageActivity extends AppCompatActivity {
+
+    private Button newAccountBT;
+    private Button haveAccountBT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        newAccountBT = (Button) findViewById(R.id.button_startPage_newAccount);
+        haveAccountBT = (Button) findViewById(R.id.button_startPage_haveAccount);
+
+        newAccountBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent start_register = new Intent(StartPageActivity.this,registerActivity.class);
+                startActivity(start_register);
+            }
+        });
+
+        haveAccountBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent start_login = new Intent(StartPageActivity.this,loginActivity.class);
+                startActivity(start_login);
+            }
+        });
+
+
     }
 }
